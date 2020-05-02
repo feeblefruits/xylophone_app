937ce0594bda73898f85ae25d6c272eaddb6b017
Jacques Coetzee (data) <j.coetzee0@gmail.com>
2020-05-02 12:40:46 +0200

    Added guitar notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MyApp());
 
+// define StatelessWidget
+
 class MyApp extends StatelessWidget {
+//  define playSound function
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({int soundNumber, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(soundNumber: 1, color: Colors.red),
+              buildKey(soundNumber: 2, color: Colors.orange),
+              buildKey(soundNumber: 3, color: Colors.yellow),
+              buildKey(soundNumber: 4, color: Colors.teal),
+              buildKey(soundNumber: 5, color: Colors.green),
+              buildKey(soundNumber: 6, color: Colors.blue),
+              buildKey(soundNumber: 7, color: Colors.purple),
+            ],
+          ),
         ),
       ),
     );
